5b0539e50296e30fef7ea6adb7cbb9223372d2f2
alziputra <alziputra12@gmail.com>
2023-11-28 19:17:02 +0700

    modification dsahboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaUserGroup, FaNewspaper, FaVideo, FaSignsPost } from "react-icons/fa6";
+import { Checkbox, Table } from "flowbite-react";
 
 const Dashboard = () => {
   return (
@@ -12,8 +13,8 @@ const Dashboard = () => {
           <div className="items-stretch flex w-full gap-5 mt-10 px-5 max-md:max-w-full max-md:flex-wrap max-md:justify-center">
             <div className="justify-end border border-[color:var(--neutral-700,#D1D9E2)] shadow-sm bg-slate-50 flex grow basis-[0%] flex-col px-5 py-4 rounded-xl border-solid items-end">
               <div className="items-stretch flex w-[213px] max-w-full justify-between gap-5">
-                <div className="justify-center items-center bg-gray-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
-                  <FaUserGroup className="aspect-square object-contain object-center w-8 overflow-hidden" />
+                <div className="justify-center items-center bg-green-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
+                  <FaUserGroup className="aspect-square object-contain object-center w-8 overflow-hidden text-green-500" />
                 </div>
                 <div className="justify-center items-stretch flex grow basis-[0%] flex-col">
                   <div className="text-black text-opacity-50 text-sm font-medium leading-5 tracking-normal">Users</div>
@@ -23,8 +24,8 @@ const Dashboard = () => {
             </div>
             <div className="justify-end border border-[color:var(--neutral-700,#D1D9E2)] shadow-sm bg-slate-50 flex grow basis-[0%] flex-col px-5 py-4 rounded-xl border-solid items-end">
               <div className="items-stretch flex w-[213px] max-w-full justify-between gap-5">
-                <div className="justify-center items-center bg-gray-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
-                  <FaNewspaper className="aspect-square object-contain object-center w-8 overflow-hidden" />
+                <div className="justify-center items-center bg-green-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
+                  <FaNewspaper className="aspect-square object-contain object-center w-8 overflow-hidden text-green-500" />
                 </div>
                 <div className="justify-center items-stretch flex grow basis-[0%] flex-col">
                   <div className="text-black text-opacity-50 text-sm font-medium leading-5 tracking-normal">News</div>
@@ -34,8 +35,8 @@ const Dashboard = () => {
             </div>
             <div className="justify-end border border-[color:var(--neutral-700,#D1D9E2)] shadow-sm bg-slate-50 flex grow basis-[0%] flex-col px-5 py-4 rounded-xl border-solid items-end">
               <div className="items-stretch flex w-[213px] max-w-full justify-between gap-5">
-                <div className="justify-center items-center bg-gray-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
-                  <FaVideo loading="lazy" className="aspect-square object-contain object-center w-8 overflow-hidden" />
+                <div className="justify-center items-center bg-green-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
+                  <FaVideo loading="lazy" className="aspect-square object-contain object-center w-8 overflow-hidden text-green-500" />
                 </div>
                 <div className="justify-center items-stretch flex grow basis-[0%] flex-col">
                   <div className="text-black text-opacity-50 text-sm font-medium leading-5 tracking-normal">Videos</div>
@@ -45,8 +46,8 @@ const Dashboard = () => {
             </div>
             <div className="justify-end border border-[color:var(--neutral-700,#D1D9E2)] shadow-sm bg-slate-50 flex grow basis-[0%] flex-col px-5 py-4 rounded-xl border-solid items-end">
               <div className="items-stretch flex w-[213px] max-w-full justify-between gap-5">
-                <div className="justify-center items-center bg-gray-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
-                  <FaSignsPost loading="lazy" className="aspect-square object-contain object-center w-8 overflow-hidden" />
+                <div className="justify-center items-center bg-green-200 flex aspect-[1.0408163265306123] flex-col px-2.5 py-2.5 rounded-[50px]">
+                  <FaSignsPost loading="lazy" className="aspect-square object-contain object-center w-8 overflow-hidden text-green-500" />
                 </div>
                 <div className="justify-center items-stretch flex grow basis-[0%] flex-col">
                   <div className="text-black text-opacity-50 text-sm font-medium leading-5 tracking-normal">Post</div>
@@ -68,10 +69,79 @@ const Dashboard = () => {
               </div>
             </div>
             {/* table */}
+            <div className="overflow-x-auto">
+              <Table hoverable>
+                <Table.Head>
+                  <Table.HeadCell className="p-4">
+                    <Checkbox />
+                  </Table.HeadCell>
+                  <Table.HeadCell>Username</Table.HeadCell>
+                  <Table.HeadCell>Post</Table.HeadCell>
+                  <Table.HeadCell>Reason</Table.HeadCell>
+                  <Table.HeadCell>Date</Table.HeadCell>
+                  <Table.HeadCell>
+                    <span className="sr-only">Edit</span>
+                  </Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell className="p-4">
+                      <Checkbox />
+                    </Table.Cell>
+                    <Table.Cell className="flex items-center gap-6 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/Jese%20Leos.png?updatedAt=1697535830098" className="w-8 h-8 rounded-full" alt="user photo" />
+                      <h1>John Doe</h1>
+                    </Table.Cell>
+                    <Table.Cell>Mari bergandengan tangan dalam menjaga kebersihan bumi ini da...</Table.Cell>
+                    <Table.Cell>Spam</Table.Cell>
+                    <Table.Cell>7 September 2023, 23:07</Table.Cell>
+                    <Table.Cell>
+                      <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                        Edit
+                      </a>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell className="p-4">
+                      <Checkbox />
+                    </Table.Cell>
+                    <Table.Cell className="flex items-center gap-6 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/Jese%20Leos.png?updatedAt=1697535830098" className="w-8 h-8 rounded-full" alt="user photo" />
+                      <h1>John Doe</h1>
+                    </Table.Cell>
+                    <Table.Cell>Mari bergandengan tangan dalam menjaga kebersihan bumi ini da...</Table.Cell>
+                    <Table.Cell>Spam</Table.Cell>
+                    <Table.Cell>7 September 2023, 23:07</Table.Cell>
+                    <Table.Cell>
+                      <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                        Edit
+                      </a>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell className="p-4">
+                      <Checkbox />
+                    </Table.Cell>
+                    <Table.Cell className="flex items-center gap-6 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/Jese%20Leos.png?updatedAt=1697535830098" className="w-8 h-8 rounded-full" alt="user photo" />
+                      <h1>John Doe</h1>
+                    </Table.Cell>
+                    <Table.Cell>Mari bergandengan tangan dalam menjaga kebersihan bumi ini da...</Table.Cell>
+                    <Table.Cell>Spam</Table.Cell>
+                    <Table.Cell>7 September 2023, 23:07</Table.Cell>
+                    <Table.Cell>
+                      <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                        Edit
+                      </a>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </div>
 
             {/* pagination */}
-            <nav className="mx-auto">
-              <ul className="inline-flex space-x-4 text-base h-10">
+            <nav className="mx-auto mt-9">
+              <ul className="inline-flex text-base h-10">
                 <Link>
                   <span className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     Previous
