f915dc966ccc88cfd84dc1836b54f926e9468939
alziputra <alziputra12@gmail.com>
2023-11-29 11:18:23 +0700

    modification dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaUserGroup, FaNewspaper, FaVideo, FaSignsPost } from "react-icons/fa6";
+import { FaUserGroup, FaNewspaper, FaVideo, FaSignsPost, FaSistrix } from "react-icons/fa6";
 import { Checkbox, Table } from "flowbite-react";
 
 const Dashboard = () => {
@@ -60,10 +60,21 @@ const Dashboard = () => {
             <div className="items-stretch border-b-[color:var(--neutral-700,#D1D9E2)] bg-slate-50 flex w-full flex-col px-5 py-4 border-b border-solid max-md:max-w-full">
               <div className="items-center flex w-full justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
                 <div className="text-black text-base font-medium leading-5 tracking-normal my-auto">Reported Post by Users</div>
-                <div className="items-center border border-[color:var(--neutral-5,#D9D9D9)] bg-white self-stretch flex justify-between gap-5 pl-3 rounded-sm border-solid">
-                  <div className="text-black text-opacity-30 text-sm leading-6 my-auto">input search text</div>
-                  <div className="justify-center items-center self-stretch border border-[color:var(--neutral-5,#D9D9D9)] flex aspect-square flex-col p-2.5 border-solid">
-                    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/972270d0-6d25-4e69-8316-d024d9ef57c4?" className="aspect-square object-contain object-center w-3.5 overflow-hidden" />
+                <div className="items-center bg-white self-stretch flex justify-between gap-5 pl-3">
+                  <div className="relative w-full">
+                    <input
+                      type="text"
+                      id="search-dropdown"
+                      className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-green-50 rounded-lg border-2 border-green-300 focus:ring-2 focus:border-green-500 dark:bg-green-700 dark:border-green-600 dark:placeholder-green-400 dark:text-white dark:focus:border-green-500"
+                      placeholder="Search"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                    >
+                      <FaSistrix />
+                    </button>
                   </div>
                 </div>
               </div>
@@ -141,39 +152,37 @@ const Dashboard = () => {
 
             {/* pagination */}
             <nav className="mx-auto mt-9">
-              <ul className="inline-flex text-base h-10">
+              <ul className="flex text-sm md:text-base">
                 <Link>
-                  <span className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     Previous
                   </span>
                 </Link>
                 <Link>
-                  <span className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     1
                   </span>
                 </Link>
                 <Link>
-                  <span className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     2
                   </span>
                 </Link>
                 <Link>
-                  <span aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+                  <span
+                    aria-current="page"
+                    className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                  >
                     3
                   </span>
                 </Link>
                 <Link>
-                  <span className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     4
                   </span>
                 </Link>
                 <Link>
-                  <span className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                    5
-                  </span>
-                </Link>
-                <Link>
-                  <span className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="flex items-center justify-center px-3 md:px-4 h-8 md:h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     Next
                   </span>
                 </Link>
