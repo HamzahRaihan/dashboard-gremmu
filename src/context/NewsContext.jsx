108371aae983a04e2c77db8d08fb018795b5e5f6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-03 17:01:43 +0700

    Refactor NewsContext.jsx

--- src/context/NewsContext.jsx
+++ src/context/NewsContext.jsx
@@ -1,38 +1,51 @@
 import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const NewsContext = createContext({
   newsData: [],
   handleAddNews: async () => {},
+  handleEditNews: async () => {},
   categories: [],
   loading: false,
+  newsById: [],
 });
 
 export const NewsContextProvider = ({ children }) => {
   const [newsData, setNewsData] = useState([]);
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
-  console.log('🚀 ~ file: NewsContext.jsx:13 ~ NewsContextProvider ~ categories:', categories);
+  const [newsById, setNewsById] = useState([]);
   const token = localStorage.getItem('ACCOUNT_TOKEN');
 
   const userData = JSON.parse(localStorage.getItem('ACCOUNT_DATA'));
-  console.log('🚀 ~ file: NewsContext.jsx:16 ~ NewsContextProvider ~ userData:', userData);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  console.log('🚀 ~ file: NewsContext.jsx:25 ~ NewsContextProvider ~ id:', id);
 
   const getAllNews = async () => {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news`);
-    const data = await response.json();
-    if (response.ok) {
-      setNewsData(data.data);
-    } else {
-      console.error('error');
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news`);
+      const data = await response.json();
+      if (response.ok) {
+        console.log('Data fetched', data);
+        setNewsData(data.data);
+      } else {
+        console.error('error');
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
+
   useEffect(() => {
     getAllNews();
   }, []);
 
-  const handleAddNews = async (title, description, fileUrl, catId) => {
-    console.log('🚀 ~ file: NewsContext.jsx:35 ~ handleAddNews ~ fileUrl:', fileUrl);
+  const handleAddNews = async (title, description, fileUrl, categoryId) => {
+    setLoading(true);
     try {
       const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news`, {
         method: 'POST',
@@ -41,7 +54,7 @@ export const NewsContextProvider = ({ children }) => {
           description,
           image: fileUrl,
           userId: userData.id,
-          catId,
+          categoryId,
         }),
         headers: {
           'Content-Type': 'application/json',
@@ -50,9 +63,56 @@ export const NewsContextProvider = ({ children }) => {
       });
       if (response.ok) {
         await getAllNews();
+        navigate('/news');
       }
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const getNewsByID = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news/${id}`);
+        const responseJson = await response.json();
+        setNewsById(responseJson.data);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getNewsByID();
+  }, [id]);
+
+  const handleEditNews = async (title, description, fileUrl, categoryId) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title,
+          description,
+          image: fileUrl,
+          userId: userData.id,
+          categoryId,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        await getAllNews();
+        navigate('/news');
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -74,7 +134,7 @@ export const NewsContextProvider = ({ children }) => {
     getCategories();
   }, []);
 
-  return <NewsContext.Provider value={{ newsData, handleAddNews, categories }}>{children}</NewsContext.Provider>;
+  return <NewsContext.Provider value={{ newsData, handleAddNews, categories, loading, newsById, handleEditNews }}>{children}</NewsContext.Provider>;
 };
 
 NewsContextProvider.propTypes = {
