5750247b40f44a465e88630200922e919b9de896
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-07 17:38:58 +0700

    Refactor PetitionContext to use useReducer and add new features

--- src/context/PetitionContext.jsx
+++ src/context/PetitionContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useReducer, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const PetitionContext = createContext();
+export const PetitionContext = createContext(null);
+
+export const PetitionDispatchContext = createContext(null);
 
 const PetitionContextProvider = ({ children }) => {
-  const [petitionsData, setPetitionsData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [petitions, dispatch] = useReducer(PetitionReducer, []);
+  const navigate = useNavigate();
+  console.log('🚀 ~ file: PetitionContext.jsx:10 ~ PetitionContextProvider ~ petitions:', petitions);
+
+  const userData = JSON.parse(localStorage.getItem('ACCOUNT_DATA'));
 
   useEffect(() => {
     setLoading(true);
@@ -13,7 +20,7 @@ const PetitionContextProvider = ({ children }) => {
         const response = await fetch(`${import.meta.env.VITE_BASE_URL}/petitions`);
         if (response.ok) {
           const responseJson = await response.json();
-          setPetitionsData(responseJson.data);
+          dispatch({ type: 'SET_PETITION', petitions: responseJson.data });
         }
       } catch (error) {
         console.error(error);
@@ -24,44 +31,60 @@ const PetitionContextProvider = ({ children }) => {
     getPetitions();
   }, []);
 
-  return <PetitionContext.Provider>{children}</PetitionContext.Provider>;
+  const postPetition = async ({ title, description, image }) => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/petitions`, {
+        method: 'POST',
+        body: JSON.stringify({ userId: userData.id, title, description, image }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const responseJson = await response.json();
+        navigate('/petitions');
+        dispatch({ type: 'ADD_PETITION', newPetition: responseJson.data });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log('Petitions State:', petitions);
+  }, [petitions]);
+
+  return (
+    <PetitionContext.Provider value={{ petitions, loading, postPetition }}>
+      <PetitionDispatchContext.Provider value={dispatch}>{children}</PetitionDispatchContext.Provider>
+    </PetitionContext.Provider>
+  );
 };
 
 export default PetitionContextProvider;
 
 const token = localStorage.getItem('ACCOUNT_TOKEN');
 
-const postPetition = async (newPetition) => {
-  try {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/petitions`, {
-      method: 'POST',
-      body: JSON.stringify({
-        newPetition,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+export const usePetitionDispatch = () => {
+  return useContext(PetitionDispatchContext);
 };
 
-const PetitionReducer = (state, action) => {
+export const usePetition = () => {
+  return useContext(PetitionContext);
+};
+
+const PetitionReducer = (petitions, action) => {
   switch (action.type) {
-    case 'ADD': {
-      const newPetition = {
-        userId: action.userId,
-        title: action.title,
-        description: action.description,
-        image: action.image,
-      };
-
-      postPetition(newPetition);
-
-      return [...state, newPetition];
+    case 'SET_PETITION':
+      return action.petitions;
+    case 'ADD_PETITION': {
+      return action.newPetition;
     }
+    default:
+      return petitions;
   }
 };
