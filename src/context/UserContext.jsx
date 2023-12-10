e81937159ce9dab37c19fcb4aa86232e2d895abb
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-10 20:32:19 +0700

    fetch user data using reducer

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,15 +1,51 @@
 import PropTypes from 'prop-types';
-import { createContext } from 'react';
+import { createContext, useEffect, useReducer, useState } from 'react';
 
 export const UserContext = createContext(null);
 
 export const UserDispatchContext = createContext(null);
 
 const UserContextProvider = ({ children }) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const [users, dispatch] = useReducer(UserReducer, []);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getAllUsers = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/users`);
+        if (response.ok) {
+          const responseJson = await response.json();
+          dispatch({ type: 'SET_USER', users: responseJson.data });
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    console.log('users data fetched', users);
+  }, [users]);
+
+  return (
+    <UserContext.Provider value={{ users, loading }}>
+      <UserDispatchContext.Provider value={dispatch}>{children}</UserDispatchContext.Provider>
+    </UserContext.Provider>
+  );
 };
 
 export default UserContextProvider;
+
+const UserReducer = (users, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return action.users;
+  }
+};
 
 UserContextProvider.propTypes = {
   children: PropTypes.node,
